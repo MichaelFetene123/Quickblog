62ca009291c40a97f6e7a115d3f7357de500cd92
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-08-09 12:31:30 -0700

    NewsLetter.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Navbar from "./../components/Navbar";
 import Header from "./../components/Header";
-import BlogList from "./../components/BlogList"; 
+import BlogList from "./../components/BlogList";
+import NewsLetter from './../components/NewsLetter';
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
       <BlogList />
+      <NewsLetter/>
     </div>
   );
 };
