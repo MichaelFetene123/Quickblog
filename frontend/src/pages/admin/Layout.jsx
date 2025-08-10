35276fdedcf5e98eab3892f5de8f09968c676ff1
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-08-10 15:02:18 -0700

    Update Layout.jsx

--- frontend/src/pages/admin/Layout.jsx
+++ frontend/src/pages/admin/Layout.jsx
@@ -9,20 +9,25 @@ const Layout = () => {
     navigate("/");
   }
   return (
-    <div className='flex items-center justify-between py-2 h-[70px] px-4 sm:px-12 border-2 border-gray-200'>
-      <img
-        src={assets.logo}
-        alt=""
-        className="w-32 sm:w-40 cursor-pointer"
-        onClick={() => navigate("/")}
-      />
-      <button
-        onClick={logout}
-        className="text-sm px-8 py-2 bg-primary text-white rounded-full cursor-pointer"
-      >
-        Logout
-      </button>
-    </div>
+    <>
+      <div className="flex items-center justify-between py-2 h-[70px] px-4 sm:px-12 border-2 border-gray-200">
+        <img
+          src={assets.logo}
+          alt=""
+          className="w-32 sm:w-40 cursor-pointer"
+          onClick={() => navigate("/")}
+        />
+        <button
+          onClick={logout}
+          className="text-sm px-8 py-2 bg-primary text-white rounded-full cursor-pointer"
+        >
+          Logout
+        </button>
+      </div>
+      <div className='flex h-[calc(100vh-70px)]'>
+        <div>sidebar</div>
+      </div>
+    </>
   );
 }
 
