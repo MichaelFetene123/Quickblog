dc4a74cbf6a12baa949306a26a50f8f1606a8786
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-08-10 16:50:40 -0700

    Add Blog Lists link to admin sidebar
    Introduces a new navigation link in the admin sidebar for accessing the Blog Lists page, including an icon and label for improved usability.

--- frontend/src/components/admin/Sidebar.jsx
+++ frontend/src/components/admin/Sidebar.jsx
@@ -13,6 +13,10 @@ const Sidebar = () => {
               <img src={assets.add_icon} alt="" className='min-w-4 w-5' />
               <p className='hidden md:inline-block'>Add Blog</p>
           </NavLink>
+          <NavLink  to='/admin/listblog' className={({isActive}) =>` flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-64 cursor-pointer ${isActive && "bg-primary/10 border-r-4 border-primary"}`}>
+              <img src={assets.list_icon} alt="" className='min-w-4 w-5' />
+              <p className='hidden md:inline-block'>Blog Lists</p>
+          </NavLink>
     </div>
   )
 }
