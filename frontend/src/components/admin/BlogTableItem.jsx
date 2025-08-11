325060f65879d50609ca619ca6c2512a0a599637
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-08-11 16:43:42 -0700

    Enhance BlogTableItem to display blog details
    BlogTableItem now accepts blog, fetchBlogs, and index as props and renders a table row with blog title, creation date, and publication status. This improves the component's utility for displaying blog data in an admin table.

--- frontend/src/components/admin/BlogTableItem.jsx
+++ frontend/src/components/admin/BlogTableItem.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
-const BlogTableItem = () => {
+const BlogTableItem = ({ blog, fetchBlogs, index }) => {
+
+  const { title, createdAt } = blog;
+  const BlogDate = new Date(createdAt)
+
   return (
-    <div>
-       
-    </div>
-  )
+    <tr className="border-y border-gray-300">
+      <th className="px-2 py-4">{index}</th>
+      <td className="px-2 py-4">{title}</td>
+      <td className="px-2 py-4 max-sm:hidden">{BlogDate.toDateString()}</td>
+      <td className="px-2 py-4 max-sm:hidden">
+        <p className={`${blog.isPublished ? 'text-green-600' : 'text-orange-700'}`} >{blog.isPublished ? 'Published' : 'Unpublished'}</p>
+      </td>
+    </tr>
+  );
 }
 
 export default BlogTableItem
