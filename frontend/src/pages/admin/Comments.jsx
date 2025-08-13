be112cb6a0a0c5aca81f3e7c6a50d50d54746c96
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-08-13 06:13:05 -0700

    Initialize comments state and fetch logic
    Added useState and useEffect hooks to manage and fetch comments data from a local asset. Prepared the Comments page for future UI and filtering functionality.

--- frontend/src/pages/admin/Comments.jsx
+++ frontend/src/pages/admin/Comments.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import React,{useState,useEffect} from 'react'
+import {comments_data} from './../../assets/assets';
 const Comments = () => {
-  return (
-    <div>Comments</div>
-  )
+  const [comments, setComments] = useState([]);
+  const [filter, setFilter] = useState("Not Approved");
+
+ const  fetchComments = async () => {
+   setComments(comments_data);
+  }
+  useEffect(() => {
+    fetchComments();
+  }, [])
+  
+  return <div className="flex-1 pt-5 px-5 sm:pt-12 sm:pl-16 bg-blue-50/50">
+    
+  </div>;
 }
 
 export default Comments
