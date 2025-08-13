844735c298b39cae6d14b7073463d57c3d17db5a
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-08-13 11:16:17 -0700

    Add image and title input to AddBlog page
    Introduced useState to manage image and title state in AddBlog. Added file input for thumbnail upload with preview and a text input for blog title.

--- frontend/src/pages/admin/AddBlog.jsx
+++ frontend/src/pages/admin/AddBlog.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import { assets } from './../../assets/assets';
 
 const AddBlog = () => {
@@ -13,17 +13,34 @@ const AddBlog = () => {
     e.preventDefault();
   }
   return (
-    <form onSubmit={onSubmitHandler} className="flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll ">
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll "
+    >
       <div className="bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded">
         <p>Upload thumbnail</p>
         <label htmlFor="image">
           <img
-            src={assets.upload_area}
+            src={!image ? assets.upload_area : URL.createObjectURL(image)}
             alt=""
             className="mt-2 h-16 rounded cursor-pointer"
           />
-          <input type="file" id="image" hidden required />
+          <input
+            onChange={(e) => setImage(e.target.files[0])}
+            type="file"
+            id="image"
+            hidden
+            required
+          />
         </label>
+
+        <p className="mt-4">Blog title</p>
+        <input
+          type="text"
+          placeholder="Type here "
+          className="outline-none border border-gray-300 rounded mt-2 p-2 max-w-lg w-full "
+          onChange={(e)=> setTitle(e.target.value)} value={title}
+        />
       </div>
     </form>
   );
