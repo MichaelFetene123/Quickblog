22e832987259266dac1e6cbfa44e75bb5e5a61e8
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-08-13 10:59:39 -0700

    Add blog creation form UI to AddBlog page
    Introduces a form layout for adding a new blog post, including fields for image upload, title, subtitle, category, and publish status. Sets up initial state management and a submit handler for future functionality.

--- frontend/src/pages/admin/AddBlog.jsx
+++ frontend/src/pages/admin/AddBlog.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { assets } from './../../assets/assets';
 
 const AddBlog = () => {
-  return (
-    <div>AddBlog</div>
-  )
-}
 
-export default AddBlog
+  const [image, setImage] = useState(false)
+  const [title, setTitle] = useState('')
+  const [subTitle, setSubTitle] = useState('')
+  const [category, setCategory] = useState('startup')
+  const [isPublished, setIsPublished] = useState(false)
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  }
+  return (
+    <form onSubmit={onSubmitHandler} className="flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll ">
+      <div className="bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded">
+        <p>Upload thumbnail</p>
+        <label htmlFor="image">
+          <img
+            src={assets.upload_area}
+            alt=""
+            className="mt-2 h-16 rounded cursor-pointer"
+          />
+          <input type="file" id="image" hidden required />
+        </label>
+      </div>
+    </form>
+  );
+};
+
+export default AddBlog;
