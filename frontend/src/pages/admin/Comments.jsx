8ab88bf991fd9518ec62e801f0b40ac5cc70f3f5
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-08-13 06:42:27 -0700

    Add header to admin comments page

--- frontend/src/pages/admin/Comments.jsx
+++ frontend/src/pages/admin/Comments.jsx
@@ -12,7 +12,9 @@ const Comments = () => {
   }, [])
   
   return <div className="flex-1 pt-5 px-5 sm:pt-12 sm:pl-16 bg-blue-50/50">
-    
+    <div className='flex justify-between items-center max-w-3xl'>
+      <h1>Comments</h1>
+    </div>
   </div>;
 }
 
