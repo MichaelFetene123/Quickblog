61b54ce1cb6e2e10113ba201e64f4571fd7e3ff3
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-08-13 05:43:30 -0700

    Update ListBlog.jsx

--- frontend/src/pages/admin/ListBlog.jsx
+++ frontend/src/pages/admin/ListBlog.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { blog_data } from './../../assets/assets';
 
 const ListBlog = () => {
+
+  const [blog, setBlog] = useState([]);
+  
+  const fetchBlogs = async () => {
+    setBlog(blog_data);
+  };
+
+  useEffect(() => {
+    
+  fetchBlogs()
+    
+  }, [])
+  
   return (
-    <div>ListBlog</div>
+    <div className='flex-1 pt-5 sm:pt-12 sm:pl-16 bg-blue-50/50 '>
+      <h1>All Blogs</h1>
+      <div>
+        
+      </div>
+    </div>
   )
 }
 
