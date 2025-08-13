bc77a4c0a510a87069d6425316311fdba7c91952
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-08-13 16:49:59 -0700

    Setup backend server and database connection
    Added backend/server.js to initialize the Express server and backend/configs/db.js for MongoDB connection using Mongoose. Updated .gitignore to improve env file matching. Removed backend/app.js and updated backend/package.json dependencies for backend setup.

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import 'dotenv/config'
+import cors from 'cors';
+import  connectDB  from './configs/db.js';
+
+const app = express();
+
+await connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("API is working!");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+export default app;
