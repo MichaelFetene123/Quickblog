d140516ce68b6609eeebf22e8e41fdaad429bb77
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-08-13 06:50:48 -0700

    Add Approved filter button to Comments page

--- frontend/src/pages/admin/Comments.jsx
+++ frontend/src/pages/admin/Comments.jsx
@@ -14,6 +14,9 @@ const Comments = () => {
   return <div className="flex-1 pt-5 px-5 sm:pt-12 sm:pl-16 bg-blue-50/50">
     <div className='flex justify-between items-center max-w-3xl'>
       <h1>Comments</h1>
+      <div className='flex gap-4'>
+<button className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs ${filter === 'Approved' ? 'text-primary' : 'text-gray-700'}`}>Approved</button>
+      </div>
     </div>
   </div>;
 }
