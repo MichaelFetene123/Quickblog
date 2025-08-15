dfe480a2e8cdb59a1b64a94663b96140af7180c3
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-08-15 16:32:44 -0700

    Update AdminController.js

--- backend/controllers/AdminController.js
+++ backend/controllers/AdminController.js
@@ -4,7 +4,7 @@ export const adminLogin = async (req, res) => {
     if (
       email !== process.env.ADMIN_EMAIL ||
       password !== process.env.ADMIN_PASSWORD
-    ) {
+    ) {return res.json({success: false ,message:'Invalid Credentials'})
     }
   } catch (e) {}
 };
