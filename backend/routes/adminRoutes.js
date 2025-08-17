76c9e52857da18ebe30e2ee9496a00df45e4a1e6
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-08-17 08:14:38 -0700

    Add admin routes for blogs and comments
    Added GET endpoints '/comments' and '/blogs' to AdminRouter, protected by auth middleware, for retrieving all comments and blogs for admin. Imports for controllers and middleware were updated accordingly.

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express'
-import { adminLogin } from './../controllers/AdminController.js';
+import {adminLogin} from "./../controllers/AdminController.js";
+import { getAllBlogsAdmin, getAllComments } from "./../controllers/blogController.js";
+import auth from "./../middleware/auth.js";
+
 
 
 const AdminRouter = express.Router()
 
 AdminRouter.post('/login', adminLogin)
+AdminRouter.get("/comments",auth, getAllComments);
+AdminRouter.get("/blogs", auth, getAllBlogsAdmin);
 
 export default AdminRouter;
