ebfd8a3a4ab8eca784fe6ed6811fbcfe25a62f72
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-08-19 05:33:11 -0700

    Add approve comment functionality to admin table
    Introduced an approveComment function in CommentTableItem.jsx to allow admins to approve comments via an API call. Success and error feedback is provided using toast notifications, and the comments list is refreshed upon approval.

--- frontend/src/components/admin/CommentTableItem.jsx
+++ frontend/src/components/admin/CommentTableItem.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import { assets } from './../../assets/assets';
 import { useAppContext } from '../../context/AppContext.jsx';
+import { toast } from 'react-hot-toast';
 
 const CommentTableItem = ({ comment, fetchComments }) => {
 
     const { blog, createdAt, _id: id,name,content} = comment;
     const BlogDate = new Date(createdAt);
 
-  const {axios} = useAppContext();
+  const { axios } = useAppContext();
+
+  const approveComment = async () => {
+    try {
+      const { data } = await axios.post(`/api/admin/approve-comment`, { id:_id});
+      if (data.success) {
+        toast.success(data.message);
+        fetchComments();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   
+
   return (
     <tr className="order-y border-gray-300">
       <td className="px-6 py-4">
